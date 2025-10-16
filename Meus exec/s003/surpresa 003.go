1551a10d1dd08759330e8f6918c2ee73eb6a0c30
Raul Santos <kaiquesato12@hotmail.com>
2025-10-15 23:59:54 -0300

    mais um teste
    teste

--- Meus exec/s003/surpresa 003.go
+++ Meus exec/s003/surpresa 003.go
@@ -6,11 +6,5 @@ func main() {
 	for a := 33; a <= 122; a++ {
 		fmt.Printf("%#U \n", a)
 	}
-	resultado:= calc (10, 20)
-	fmt.Println(resultado)
 }
 
-
-func calc(a int, b int) int {
-	return a + b
-}
